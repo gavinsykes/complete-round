c0f6b29e43536495eeb86650b5c2bb5f8f905506
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-20 12:23:29 +0100

    Adding decimal limiter for if necessary

--- src/completeRound.ts
+++ src/completeRound.ts
@@ -1,20 +1,28 @@
-function completeRound( number: number, rounding = 1, direction = "closest" ): number {
+function numberOfDecimals(number: number): number {
+  return Math.floor(number) === number
+         ? 0
+         : parseInt(number.toString().split('.')[1].length) || 0;
+}
+
+function completeRound( number: number, rounding = 1, direction = 'closest' ): number {
   let n: number = +number,
       r: number = Math.abs(+rounding),
       d: string = direction;
 
-  if (typeof n !== "number") {
-    throw new Error("You need to round a number!");
+  let maxDecimals: number = Math.max(numberOfDecimals(n),numberOfDecimals(r));
+
+  if (typeof n !== 'number') {
+    throw new Error('You need to round a number!');
   }
 
   if (n === 0) {
     return 0;
   }
   if (r === 1) {
-    if (d === "down") {
+    if (d === 'down') {
       return Math.floor(number);
     }
-    if (d === "up") {
+    if (d === 'up') {
       return Math.ceil(number);
     }
   }
@@ -30,19 +38,19 @@ function completeRound( number: number, rounding = 1, direction = "closest" ): n
               : Math.floor(n / r) * r
   };
   switch (d) {
-    case "down":
+    case 'down':
       return bounds.lower;
       break;
-    case "up":
+    case 'up':
       return bounds.upper;
       break;
-    case "towards":
+    case 'towards':
       return bounds.towards;
       break;
-    case "away":
+    case 'away':
       return bounds.away;
       break;
-    case "closest":
+    case 'closest':
       let c: number = bounds.upper;
       if (n < 0) {
         if (n - bounds.upper <= bounds.lower - n) {
@@ -52,10 +60,10 @@ function completeRound( number: number, rounding = 1, direction = "closest" ): n
       if (n - bounds.lower < bounds.upper - n) {
         c = bounds.lower;
       }
-      return c;
+      return c.toFixed(maxDecimals);
       break;
     default:
-      throw new Error(`Please select a valid rounding direction, it should default to "closest" but other options are "up", "down", "away" [from zero] and "towards" [zero].`);
+      throw new Error(`Please select a valid rounding direction, it should default to 'closest' but other options are 'up', 'down', 'away' [from zero] and 'towards' [zero].`);
       break;  
   }
 };
