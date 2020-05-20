33361ec663c2c6cf2ffc21830857ef6c105c1d9a
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-20 11:22:08 +0100

    Setting up NPM, TypeScript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  devtool : 'inline-source-map',
+  entry   : './src/index.ts',
+  mode    : 'production',
+  module  : {
+    rules : [
+      {
+        test : /\.tsx?$/,
+        use  : 'ts-loader'
+      }
+    ]
+  },
+  output  : {
+    filename      : 'main.js',
+    library       : 'complete_round',
+    libraryTarget : 'var',
+    path          : path.resolve(__dirname, 'dist')
+  },
+  resolve : {
+    extensions : [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+};
